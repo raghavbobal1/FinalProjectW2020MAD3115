b861f0c549485d9c544d961b2062890c5d3122b0
raghavbobal1 <raghavbobal001@gmail.com>
2020-03-17 04:34:43 -0400

    [Raghav] Removed text fields and added buttons

---
+++ FinalProjectzW2020MAD3115/View Controllers/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  FinalProjectzW2020MAD3115
+//
+//  Created by Raghav Bobal on 2020-03-17.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

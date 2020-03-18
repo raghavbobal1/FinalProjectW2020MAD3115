ecf2a4520c035ac3336f8b5166062b518a48a119
raghavbobal1 <raghavbobal001@gmail.com>
2020-03-18 15:39:51 -0400

    [Abhsihek] Removed extra code fro person package

--- FinalProjectzW2020MAD3115/Required Classes and extensions/Person/Customer.swift
+++ FinalProjectzW2020MAD3115/Required Classes and extensions/Person/Customer.swift
@@ -25,77 +25,10 @@ class Customer : Person
         
     }
 
-    init(id: String, firstName: String, lastName: String, gender: Gender,birthDate: Date?,age:Int, userName: String, password: String, contact: Contact)
+    init(id: String, fullName: String, lastName: String, gender: Gender,birthDate: Date?,age:Int, email: String, password: String, contact: Contact)
     {
-        super.init(id: id, firstName: firstName, lastName: lastName, gender: gender, birthDate: birthDate, userName: userName, password: password, contact: contact)
+        super.init(id: id, fullName: fullName, gender: gender, birthDate: birthDate, email: email, password: password, contact: contact)
     }
-    
-    init(customerDict: [String: Any]) throws {
-        
-        // get rents
-        guard let rents = customerDict["rentsId"] as? [String] else {
-            throw JsonValidationError.isNotValidInput(
-                className: String(describing:type(of: self)),
-                memberName: "rentsId")
-        }
-        let rentsManger = ObjectManager.getInstance()
-        var rentsObjectList = [String: VehicleRent]()
-        for rentId in rents{
-            if let rent = rentsManger.getRentById(id: rentId){
-                rentsObjectList.updateValue(rent, forKey: rentId)
-            }
-        }
-        
-        try super.init(personDict: customerDict)
-        self.vehicleRents = rentsObjectList
-    }
-    
-    
-    override func display()
-    {
-        var logger = Log()
-        print("\nID            : \(id)", to: &logger)
-        print("First Name     : \(firstName)", to: &logger)
-        print("Last Name      : \(lastName)", to: &logger)
-        print("Gender         : \(gender)", to: &logger)
-        print("Date of Birth  : \(birthDate ?? Date())", to: &logger)
-        print("Age            : \(age)", to: &logger)
-        print("Username       : \(userName)", to: &logger)
-
-    }
-    
-     func display(withBookings: Bool)
-    {
-        var logger = Log()
-        print("\nID            : \(id)", to: &logger)
-        print("First Name     : \(firstName)", to: &logger)
-        print("Last Name      : \(lastName)", to: &logger)
-        print("Gender         : \(gender)", to: &logger)
-        print("Date of Birth  : \((birthDate ?? Date()).printFormat())", to: &logger)
-        print("Age            : \(age)", to: &logger)
-        print("Username       : \(userName)", to: &logger)
-        
-        
-        if withBookings{
-            for (_,rent) in vehicleRents{
-                rent.display()
-            }
-            print("\t*********TOTAL: \(totalBill.priceFormat())*********", to: &logger)
-        }
-
-    }
-    
-  
-  func addVehicleRents(vehicleRent: VehicleRent)
-    {
-        if vehicleRents.keys.contains(vehicleRent.id)
-      {
-        print("\nERROR! VEHICLE Rent ALREADY RENTED")
-      }
-      else 
-      {
-        vehicleRents.updateValue(vehicleRent,forKey: vehicleRent.id)
-      }
-    }
+ 
 
 }

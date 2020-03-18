88fbdb867bf1554cef3485e526fb8e9dc757cc22
raghavbobal1 <raghavbobal001@gmail.com>
2020-03-18 16:46:55 -0400

    [Abhishek] added date picker in sign up page

--- FinalProjectzW2020MAD3115/View Controllers/SignUpViewController.swift
+++ FinalProjectzW2020MAD3115/View Controllers/SignUpViewController.swift
@@ -14,9 +14,11 @@ class SignUpViewController: UIViewController
     @IBOutlet weak var txtEmail: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
     @IBOutlet weak var txtMobileNumber: UITextField!
+    @IBOutlet weak var txtDOB: UIDatePicker!
     override func viewDidLoad()
        {
            super.viewDidLoad()
+           self.txtDOB.setInputViewDatePicker(target: self, selector: #selector(tapDone))
        }
     
    @IBAction func backBtn(_ sender: Any)
@@ -24,8 +26,68 @@ class SignUpViewController: UIViewController
         navigationController?.popViewController(animated: true)
     }
     
-    
-    
+    @IBAction func signUpBtn(_ sender: Any)
+      {
+          let txtNumber = txtMobileNumber.text!
+          let txtmail  = txtEmail.text!
+          
+          if(txtfullName.text == "")
+          {
+              let alertController = UIAlertController(title:"Error", message: "Invalid entry, name left blank", preferredStyle: .alert)
+              alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+              self.present(alertController, animated: true, completion: nil)
+              return
+          }
+          
+          if Validations.email(email: txtmail == false && Validations.mobileNumber(number: txtNumber) == false)
+          {
+              let alertController = UIAlertController(title:"Error", message: "Invalid email ID and phone number", preferredStyle: .alert)
+              alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+              self.present(alertController, animated: true, completion: nil)
+              return
+          }
+          
+          if(Validations.email(email: txtmail) == false)
+              {
+                  let alertController = UIAlertController(title:"Error", message: "Invalid email ID", preferredStyle: .alert)
+                  alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+                  self.present(alertController, animated: true, completion: nil)
+                  return
+              }
+          
+          if(Validations.mobileNumber(number: txtNumber) == false)
+          {
+              let alertController = UIAlertController(title:"Error", message: "Invalid phone number", preferredStyle: .alert)
+              alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+              self.present(alertController, animated: true, completion: nil)
+              return
+          }
+          let c = Customer(customerId: txtNewCustomerId.text!, name: txtNewCustomerName.text!, email: txtNewCustomerEmail.text!, userName: txtNewCustomerUserName.text!, password: txtNewCustomerPassword.text!, mobileNumber: txtNewCustomerNumber.text!, dob: txtNewCustomerDOB.text!)
+          
+              DataRepository.getInstance().addCustomer(customer: c)
+          
+          
+          let alertController = UIAlertController(title: "Success", message: "Customer Added", preferredStyle: .alert)
+              alertController.addAction(UIAlertAction(title: "OK", style: .default, handler:
+              {action in self.dismissView()}))
+                  self.present(alertController, animated: true, completion: nil)
+       }
+          func dismissView()
+          {
+              navigationController?.popViewController(animated: true)
+          }
+        
+        @objc func tapDone()
+        {
+          if let datePicker = self.txtNewCustomerDOB.inputView as? UIDatePicker
+          {
+                let dateformatter = DateFormatter()
+                dateformatter.dateStyle = .medium
+                self.txtNewCustomerDOB.text = dateformatter.string(from: datePicker.date)
+            }
+            self.txtNewCustomerDOB.resignFirstResponder()
+          }
+    }
     
     
 }

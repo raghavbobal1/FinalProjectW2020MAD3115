9bc229f5e935a8cdc3e7485654051e153fd74676
raghavbobal1 <raghavbobal001@gmail.com>
2020-03-19 17:39:53 -0400

    [Raghav] Added a customerviewcontroller

---
+++ FinalProjectzW2020MAD3115/View Controllers/CustomerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomerViewController.swift
+//  FinalProjectzW2020MAD3115
+//
+//  Created by Raghav Bobal on 2020-03-19.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import UIKit
+
+class CustomerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

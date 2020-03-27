935938bfb3395f6ea078acacf99547a551fcae6e
prana <abhisheksj35@gmail.com>
2020-03-27 11:16:35 -0400

    Added vehicle rent detailk

--- FinalProjectzW2020MAD3115/View Controllers/Rent/RentDetailViewController.swift
+++ FinalProjectzW2020MAD3115/View Controllers/Rent/RentDetailViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class RentDetailViewController: UIViewController {
+    @IBOutlet weak var labelKMDriven: UILabel!
+    @IBOutlet weak var labelVehicleId: UILabel!
+    @IBOutlet weak var labelTotalBill: UILabel!
+    @IBOutlet weak var labelDays: UILabel!
+    @IBOutlet weak var labelEndDate: UILabel!
+    @IBOutlet weak var labelStartDate: UILabel!
     var vehicleRent: VehicleRent?
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +22,15 @@ class RentDetailViewController: UIViewController {
         guard let vehicleRent = self.vehicleRent else {
                     return
                 }
+        
+        self.labelStartDate.text = vehicleRent.rentStartDate.printFormat()
+        self.labelEndDate.text = vehicleRent.rentEndDate.printFormat()
+        self.labelDays.text = "\(vehicleRent.calculateNumberOfDays()) Days"
+        
+        self.labelTotalBill.text = vehicleRent.calculateTotalBill().priceFormat()
+        self.labelVehicleId.text = vehicleRent.vehicle.vehicleId
+        
+        self.labelKMDriven.text = vehicleRent.kmDriven.distanceFormat()
         // Do any additional setup after loading the view.
     }
     

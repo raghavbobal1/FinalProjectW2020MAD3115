83c8a6d0e79058c23e58c578e2a68f65a862cdc2
prana <prakashrana9847067614@gmail.com>
2020-03-27 01:12:33 -0400

    Added animation in tab bar

---
+++ FinalProjectzW2020MAD3115/View Controllers/CustomTabBarViewController.swift
@@ -0,0 +1,53 @@
+//
+//  CustomTabBarViewController.swift
+//  FinalProjectzW2020MAD3115
+//
+//  Created by Prakash on 2020-03-27.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import UIKit
+
+class MySubclassedTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      delegate = self
+    }
+    
+    private var bounceAnimation: CAKeyframeAnimation = {
+        let bounceAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
+        bounceAnimation.values = [1.0, 1.4, 0.9, 1.02, 1.0]
+        bounceAnimation.duration = TimeInterval(0.3)
+        bounceAnimation.calculationMode = CAAnimationCalculationMode.cubic
+        return bounceAnimation
+    }()
+
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        // find index if the selected tab bar item, then find the corresponding view and get its image, the view position is offset by 1 because the first item is the background (at least in this case)
+        guard let idx = tabBar.items?.index(of: item), tabBar.subviews.count > idx + 1, let imageView = tabBar.subviews[idx + 1].subviews.compactMap({ $0 as? UIImageView }).first else {
+        return
+        }
+
+        imageView.layer.add(bounceAnimation, forKey: nil)
+    }
+}
+
+extension MySubclassedTabBarController: UITabBarControllerDelegate  {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+
+        guard let fromView = selectedViewController?.view, let toView = viewController.view else {
+          return false // Make sure you want this as false
+        }
+
+        if fromView != toView {
+            UIView.transition(from: fromView, to: toView, duration: 0.3, options: [.transitionCrossDissolve], completion: nil)
+        }
+
+        return true
+    }
+    
+    
+}
+
+   
--- FinalProjectzW2020MAD3115/View Controllers/SelectionNavViewController.swift
+++ FinalProjectzW2020MAD3115/View Controllers/SelectionNavViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SelectionNavViewController: UITabBarController {
+class SelectionNavViewController: MySubclassedTabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

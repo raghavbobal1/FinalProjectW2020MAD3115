dab584562c939e4d4b81297ea6a0ba4bb2415141
prana <prakashrana9847067614@gmail.com>
2020-03-27 04:09:43 -0400

    Added extension to animate to color

--- FinalProjectzW2020MAD3115/Model/Extensions/Extension+UITextField .swift
+++ FinalProjectzW2020MAD3115/Model/Extensions/Extension+UITextField .swift
@@ -23,4 +23,26 @@ extension UITextField
         self.resignFirstResponder()
     }
     
+    func animateToColor(selectedColor: UIColor) {
+        let color = CABasicAnimation(keyPath: "borderColor")
+        
+        color.fromValue = self.layer.borderColor
+        
+        color.toValue = selectedColor
+        
+        color.duration = 0.7
+        color.repeatCount = 1
+
+        self.layer.borderColor =  selectedColor.cgColor
+        
+        if selectedColor == UIColor.black{
+            self.layer.borderWidth = 0.0
+        }
+        else{
+            self.layer.borderWidth = 1.0
+        }
+        
+        self.layer.add(color, forKey: "borderColor")
+    }
+    
 }
